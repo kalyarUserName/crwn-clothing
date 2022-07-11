5b65e4ae1d489f5831c3fc6773294a5728a4737b
kalyarUserName <ron230298@email.com>
2022-07-11 21:51:00 +0300

    feat: add sign-in component as firebase utils

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/signIn/signIn.component";
 
 const Shop = () => {
   return <h1>Shop component</h1>;
@@ -12,6 +13,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
