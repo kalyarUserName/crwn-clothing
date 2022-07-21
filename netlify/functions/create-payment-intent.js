7c1129d2e94a406097b67cd5ee98ddd21df7b578
kalyarUserName <ron230298@email.com>
2022-07-21 14:29:47 +0300

    add stripe payment

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(
+  // "sk_test_51LNfofAwlDqxZMpUZ7Qeyns8hnG4kgvuDviIdf18XRDqxZwIRpQubMheMTMBTulyOidccRX7C14EU1LOmsg7OB7w00Gk1pEn0U"
+  process.env.REACT_APP_STRIPE_SECRET_KEY
+);
 
 exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
-
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "usd",
@@ -18,6 +20,9 @@ exports.handler = async (event) => {
   } catch (error) {
     console.log({ error });
 
-    return { status: 400, body: JSON.stringify({ error }) };
+    return {
+      status: 400,
+      body: JSON.stringify({ error }),
+    };
   }
 };
--- src/components/paymentForm/paymentForm.component.jsx
+++ src/components/paymentForm/paymentForm.component.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 import {
   FormContainer,
   PaymentFormContainer,
@@ -10,17 +14,39 @@ import { BUTTON_TYPES_CLASSES } from "../button/button.component";
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
   const paymentHandler = async (event) => {
     event.preventDefault();
     if (!stripe || !elements) return;
-
+    setIsProcessingPayment(true);
     const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "post",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ amount: 10000 }),
-    }).then((res) => res.json());
-    console.log(response);
+      body: JSON.stringify({ amount: amount * 100 }),
+    }).then((res) => {
+      return res.json();
+    });
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: currentUser ? currentUser.displayName : "Guest",
+        },
+      },
+    });
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else if (paymentResult.paymentIntent.status === "succeeded") {
+      alert("Payment Successful");
+    }
+    setIsProcessingPayment(false);
   };
 
   return (
@@ -28,7 +54,10 @@ const PaymentForm = () => {
       <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment:</h2>
         <CardElement />
-        <PaymentButton buttonType={BUTTON_TYPES_CLASSES.inverted}>
+        <PaymentButton
+          isLoading={isProcessingPayment}
+          buttonType={BUTTON_TYPES_CLASSES.inverted}
+        >
           Pay Now
         </PaymentButton>
       </FormContainer>
